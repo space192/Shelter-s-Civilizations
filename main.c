4fdada3172d0bcdc973d2519bad6f47567ed5646
pierrelouispaillet <pierrelouis.paillet@edu.ece.fr>
2020-04-30 17:46:07 +0200

    Scroll fond Menu

--- main.c
+++ main.c
@@ -3,22 +3,35 @@
 int main()
 {
     lancerAllegro(1280, 1024);
+    BITMAP *fond=load_bitmap("image/menu/fondMenu.bmp",NULL);
     BITMAP *buffer = create_bitmap(SCREEN_W, SCREEN_H);
     BITMAP *image[13];
     char chaine[100];
     chaine[0] = '\0';
     int compteur=0, sauvegarde = 1, continuer = 1, tuto = 0, ecrire = 0, pos=0, clic=0, sauvegardetemp = 4;
+    float x=0;
     chargementImageMenu(image);
     int scene = 1; //valeur scene corresponde au different ecran du menu
-                   //1:Menu Principale
-                   //2:Nouvelle Partie
-                   //3:Chargement Partie
-                   //4:Score en ligne
+    //1:Menu Principale
+    //2:Nouvelle Partie
+    //3:Chargement Partie
+    //4:Score en ligne
 
     while(!key[KEY_ESC] && continuer == 1)
     {
         clear_bitmap(buffer);
-        //affichage d'une animation derriere le menu gerer par un spg ici
+
+        blit(fond,buffer,x,0,0,0,SCREEN_W, SCREEN_H);
+        x+=0.4;
+        if(x>3071)
+        {
+            blit(fond,buffer,x-4351,0,0,0,SCREEN_W, SCREEN_H);
+        }
+        if(x>=4351)
+        {
+            x=0;
+        }
+
         if(scene == 1)
         {
             MenuPrincipale(buffer, image, &scene, &compteur, sauvegarde);
@@ -38,6 +51,7 @@ int main()
         //textprintf_ex(buffer, font, 10, 10, makecol(255,255,255), -1, "x:%d et y:%d", mouse_x, mouse_y);
         blit(buffer, screen, 0,0,0,0,SCREEN_W, SCREEN_H);
     }
+    destroy_bitmap(fond);
     destroy_bitmap(buffer);
     dechargementImage(image);
     allegro_exit();
